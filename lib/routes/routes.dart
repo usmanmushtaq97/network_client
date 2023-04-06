234e7532aedf7eac7169b1a5cda087466a444c3a
usman <usmanmushtaq848@gmail.com>
2023-04-06 19:57:07 +0300

    routes updated

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -4,13 +4,28 @@
 //
 
 import 'package:flutter/material.dart';
-class Routes{
+
+class Routes {
   Routes._();
-   /// The pop means the back of one Queue
+
+  /// The pop means the back of one Queue
   /// Example if got A to B Route then by calling this method will be on A
-  static pop(BuildContext context){
+  static pop(BuildContext context) {
     Navigator.pop(context);
   }
 
+  /// The push means the push the page like go to next page  A page to B page
+  /// Example if got A to B Route then by calling this method will be on A
+  static push(Object? argument,
+      {required BuildContext context, required String routeName}) {
+    Navigator.pushNamed(context, routeName, arguments: argument);
+  }
 
+  /// The push means the push the page like go to next page  A page to B page and remove all the routes from Queue
+  /// Example if got A to B Route then by calling this method will be on A
+  static pushRemovedUntil(Object? argument, RoutePredicate routePra,
+      {required BuildContext context, required String routeName}) {
+    Navigator.pushNamedAndRemoveUntil(context, routeName, routePra,
+        arguments: argument);
+  }
 }
